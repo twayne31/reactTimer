339486a0e5bdba8d2ee0ac4880086eee74a2113c
Taylor Hampton <taylorhampton@Waynes-MacBook-Air.local>
2018-06-13 16:18:29 -0400

    add automatic test

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,10 @@
+var expect = require('expect')
+
+
+
+//describe is visible function...formats output to be more readable
+describe('App', () => {
+    it('should properly run test', () => {
+        expect(1).toBe(1);
+    })
+});
