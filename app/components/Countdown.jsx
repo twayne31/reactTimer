1b37ec5d123e2455837c276e53a78bec4789839b
Taylor Hampton <taylorhampton@Waynes-MacBook-Air.local>
2018-06-27 23:36:41 -0400

    controls components and styling

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react')
 var Clock = require('Clock')
 var CountdownForm = require('CountdownForm')
+var Controls = require('Controls')
 
 var Countdown = React.createClass({
     getInitialState: function (){
@@ -17,6 +18,12 @@ var Countdown = React.createClass({
                 case 'started':
                     this.startTimer();
                     break;
+                case 'stopped':
+                    this.setState({count: 0});
+                case 'paused':
+                    clearInterval(this.timer)
+                    this.timer = undefined;
+                    break;    
             }
         }
     },
@@ -30,6 +37,9 @@ var Countdown = React.createClass({
             });
         }, 1000);
     },
+    handleStatusChange: function (newStatus) {
+        this.setState({countdownStatus: newStatus});
+    },  
     handleSetCountdown: function (seconds){
         this.setState({
             count: seconds,
@@ -38,12 +48,19 @@ var Countdown = React.createClass({
         });
     },
     render: function (){
-        var {count} = this.state;
+        var {count, countdownStatus} = this.state;
+        var renderControlArea = () => {
+            if (countdownStatus !== 'stopped'){
+                return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+            } else {
+                return <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+            }
+        }
 
         return(
             <div>
                 <Clock totalSeconds={count}/>
-                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+                {renderControlArea()}
             </div>
         )
     }
