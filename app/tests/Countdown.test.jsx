21d91f86fff3e6af6f74db5348fc3cc7449d7f7c
Taylor Hampton <taylorhampton@Waynes-MacBook-Air.local>
2018-06-29 23:21:12 -0400

    Finalize test and add page title

--- app/tests/Countdown.test.jsx
+++ app/tests/Countdown.test.jsx
@@ -26,14 +26,34 @@ describe('Countdown', () => {
         })
         it('should never set count less than 0', (done) => {
             var countdown = TestUtils.renderIntoDocument(<Countdown/>)
-            countdown.handleSetCountdown(1);
-
-            
+            countdown.handleSetCountdown(1);   
             //asynchronous 
             setTimeout(() => {
                 expect(countdown.state.count).toBe(0);
                 done();
             }, 3001)
+        });
+        it('should pause countdown on paused status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>)
+            countdown.handleSetCountdown(3)
+            countdown.handleStatusChange('paused')
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3)
+                expect(countdown.state.countdownStatus).toBe('paused');
+                done()
+            }, 1001);
+        })
+        it('should stop countdown on stopped status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>)
+            countdown.handleSetCountdown(2)
+            countdown.handleStatusChange('stopped')
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0)
+                expect(countdown.state.countdownStatus).toBe('stopped');
+                done()
+            }, 1001);
         })
     })
 })
